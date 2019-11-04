66156941b305de6e2d292ad80876152d6940cec3
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-04 04:55:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -116,5 +116,5 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.collect {|holiday| holiday.values}
-  
+
 end
