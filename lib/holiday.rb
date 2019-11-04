7a72d8dfb5d782a7a0aa15028a33b7f138b44093
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-04 04:31:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_supplies_in_holidays(holiday_hash)
        puts "#{season.capitalize}:"
 
    event.each do |event_name, supply|
-     puts "#{event_name.capitalize}: #{supply}".flatten
+     puts "#{event_name.capitalize}: #{supply.to_s}"
 
       #each loop of every season and event, event loop with event names and supply list
   # season = season.to_s.capitalize
